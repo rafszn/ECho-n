7c97d7491d480f120907fb83523b6f41dd59e247
rafael <yhrapha@gmail.com>
2024-08-19 12:52:54 +0100

    feat: huggingFace API added

--- controllers.js
+++ controllers.js
@@ -1,65 +1,80 @@
-import OpenAi from 'openai'
-import dotenv from 'dotenv'
-import axios from 'axios'
-dotenv.config()
+import dotenv from "dotenv";
+import axios from "axios";
+import { HfInference } from "@huggingface/inference";
+dotenv.config();
 
+const hf = new HfInference(process.env.HUGGING);
 
-export async function Translator (req, res){
-  const {transcript, language} = req.body
+function cleanText(str) {
+  const newlineIndex = str.indexOf("\n");
+  const parenthesisIndex = str.indexOf("(");
+  const endIndex =
+    newlineIndex !== -1 && parenthesisIndex !== -1
+      ? Math.min(newlineIndex, parenthesisIndex)
+      : Math.max(newlineIndex, parenthesisIndex);
 
-  const openai = new OpenAi({ apiKey:process.env.OPENAI_KEY})
-
-  try {
-    const response = await openai.chat.completions.create({
-      model:'gpt-3.5-turbo',
-      messages:[
-        {
-          'role':"system",
-          'content':`
-          You will be provided with a sentence. Your tasks are to:
-          - Detect the language of the sentence
-          - translate it into ${language}
-          Do not tell me what the language is, just return the translated text.
-          `
-        },
-        {
-          'role':'user',
-          'content': transcript
-        }
-      ],
-      temperature: 0.7,
-      top_p:1
-    })
-
-    return res.status(200).json({
-      text: response.choices[0].message.content
-    })
-
-  } catch (error) {
-    console.log(error)
-    return res.status(400).json(error.message)
+  if (endIndex !== -1) {
+    return str.substring(0, endIndex).trim();
   }
-
+  return str.trim();
 }
 
+export async function Translator(req, res) {
+  const { transcript, language } = req.body;
 
+  try {
+    const responseTwo = await hf.chatCompletion({
+      model: "mistralai/Mistral-7B-Instruct-v0.2",
+      messages: [
+        {
+          role: "system",
+          content: `
+          You will be provided with a sentence. Your tasks are to:
+          
+          - translate it into ${language}.
+          - Do not tell me what the language is.
+          - Do not add any (Note) to the response, no context, no nothing.
+          - Do not say what the sentence is about.
+          - strictly return the translated sentence as a response.
+          `,
+        },
+        {
+          role: "user",
+          content: transcript,
+        },
+      ],
+      temperature: 0.1,
+      seed: 0,
+    });
 
-export async function Transcribe (req, res){
+    const cleaned = cleanText(responseTwo.choices[0].message.content);
 
-  const audio = req.file.path
+    return res.status(200).json({
+      text: cleaned,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.message);
+  }
+}
 
-   const response = await axios.post('https://api.openai.com/v1/audio/transcriptions', {
+export async function Transcribe(req, res) {
+  const audio = req.file.path;
+
+  const response = await axios.post(
+    "https://api.openai.com/v1/audio/transcriptions",
+    {
       audio,
-      model: 'whisper-1',
-      response_format: 'json'
-    },{
+      model: "whisper-1",
+      response_format: "json",
+    },
+    {
       headers: {
-        'Content-Type':'multipart/form-data',
-        'Authorization': `Bearer ${process.env.OPENAI_KEY}`
-      }
-    }
-  ) 
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${process.env.OPENAI_KEY}`,
+      },
+    },
+  );
 
-  return res.status(200).json({text:response.data.text})
-
+  return res.status(200).json({ text: response.data.text });
 }
